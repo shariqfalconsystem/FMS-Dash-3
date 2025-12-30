8aef8c742b3e2af44c638f28f64b2fe1ef1c66c8
Manish Kumar Sharma <manish.sharma@falconsystem.com>
2025-12-30 11:46:03 +0530

    final sidebar update

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -397,17 +397,17 @@ const AppSidebar: React.FC = () => {
                     : "justify-start"
                 }`}
               >
-                {isExpanded || isHovered || isMobileOpen ? (
+                {/* {isExpanded || isHovered || isMobileOpen ? (
                   "Others"
                 ) : (
                   <HorizontaLDots />
-                )}
+                )} */}
               </h2>
-              {renderMenuItems(othersItems, "others")}
+              {/* {renderMenuItems(othersItems, "others")} */}
             </div>
           </div>
         </nav>
-        {isExpanded || isHovered || isMobileOpen ? <SidebarWidget /> : null}
+        {/* {isExpanded || isHovered || isMobileOpen ? <SidebarWidget /> : null} */}
       </div>
     </aside>
   );
